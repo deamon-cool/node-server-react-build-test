96ed96a11860d80601b95acd5bffc0bfdfc0d0b9
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-06 11:08:58 +0200

    added react app build

---
+++ public/precache-manifest.efaca54ad0fa8418c5ad8060579af33e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee20f119a33d0ac44ffd0b3c28cf368c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e482e572dccf90a25042",
+    "url": "/static/css/main.2f6c2f2b.chunk.css"
+  },
+  {
+    "revision": "296dae9d99eefafa0a6e",
+    "url": "/static/js/2.7d42f2c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d42f2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e482e572dccf90a25042",
+    "url": "/static/js/main.ac1f62bc.chunk.js"
+  },
+  {
+    "revision": "54fa86d2aea86a318381",
+    "url": "/static/js/runtime-main.5c3b6bd7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
